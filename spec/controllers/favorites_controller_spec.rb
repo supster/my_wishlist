b0c48cca070654a88fb8d6c2ade8c382113ef2e8
Supatra Indhara <supatra.indhara@gmail.com>
2013-03-23 20:21:23 -0400

    update Ajax button

---
+++ spec/controllers/favorites_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe FavoritesController do
+=begin  
+  describe "creating a relationship with ajax" do
+    it "should response with success" do
+      expect do
+        xhr: :post, :create, :favorite { vendor_id: }
+      end
+    end
+  end
+=end
+end
